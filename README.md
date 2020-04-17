abfc8bc0c3b7795bb02c3a8ab9ca2ed9fdc7164c
Developer <oleksandr.trunov@lhind.dlh.de>
2020-04-17 22:17:18 +0200

    staff

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # aws_ec2_snapshots
 Example managing snapshots AWS EC2
+
+```
+pipenv run python ec2.py
+
+pipenv run python ec2.py list --project=MyProject
+```
--- ec2.py
+++ ec2.py
@@ -4,11 +4,56 @@ import click
 session = boto3.Session(profile_name="developer")
 ec2 = session.resource("ec2")
 
-@click.command()
-def get_ec2_instances():
+def list_ec2_instaces(project):
+    the_instances = []
+
+    if project:
+        filters = [{'Name': 'tag:Project', 'Values': [project]}]
+        the_instances = ec2.instances.filter(Filters = filters)
+    else:
+        the_instances = ec2.instances.all()
+    
+    return the_instances
+
+@click.group()
+def instances():
+    "Commands for instances"
+
+@instances.command("list")
+@click.option("--project", default=None, help="Omly instances for project (tag Project:<name>)")
+def get_ec2_instances(project):
     "Get all EC2 instances"
-    for i in ec2.instances.all():
-        print(",".join((i.id, i.instance_type, i.placement["AvailabilityZone"], i.state["Name"], i.public_dns_name)))
+    the_instances = list_ec2_instaces(project)
+
+    for i in the_instances:
+        tags = {t["Key"]: t["Value"] for t in i.tags or []}
+        print(",".join((
+            i.id, 
+            i.instance_type, 
+            i.placement["AvailabilityZone"], 
+            i.state["Name"], 
+            i.public_dns_name, 
+            tags.get('Project', '<no project>'))))
+
+@instances.command("stop")
+@click.option("--project", default=None, help="Omly instances for project (tag Project:<name>)")
+def stop_ec2_instances(project):
+    "Stop EC2 instances"
+    the_instances = list_ec2_instaces(project)
+
+    for i in the_instances:
+        print("Stopping {0}...".format(i.id))
+        i.stop()
+
+@instances.command("start")
+@click.option("--project", default=None, help="Omly instances for project (tag Project:<name>)")
+def stop_ec2_instances(project):
+    "Start EC2 instances"
+    the_instances = list_ec2_instaces(project)
+
+    for i in the_instances:
+        print("Starting {0}...".format(i.id))
+        i.start()
 
 if __name__ == "__main__":
-    get_ec2_instances()
+    instances()
